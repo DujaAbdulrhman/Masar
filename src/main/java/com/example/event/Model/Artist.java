a28c66977e1cae2c1da79db1588c3a9cabfdaf2e
Duja Abdurhman <134400247+DujaAbdulrhman@users.noreply.github.com>
2025-04-15 09:17:13 +0300

    Update Artist.java

--- src/main/java/com/example/event/Model/Artist.java
+++ src/main/java/com/example/event/Model/Artist.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,4 +26,11 @@ public class Artist {
     private String originCountry;
 
     private boolean featured;
+    @NotNull(message = "age is required")
+    private Integer age;
+
+    @NotEmpty(message = "gender is required")
+    @Pattern(regexp = "^(male|female)$", message = "gender must be either 'male' or 'female'")
+    private String gender;
+
 }
